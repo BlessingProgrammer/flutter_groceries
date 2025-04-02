1f5fcc7d917a8a56b725e3f96ee1fb1efcf896b6
BlessingProgrammer <blessingprogrammer@gmail.com>
2025-04-03 00:22:29 +0700

    feat: verification screen, number screen

--- lib/layers/core/presentation/number_screen.dart
+++ lib/layers/core/presentation/number_screen.dart
@@ -20,7 +20,77 @@ class NumberScreenState extends State<StatefulWidget> {
           onPressed: () {},
           backgroundColor: algae,
           foregroundColor: Colors.white,
-          child: const Icon(Icons.add),
+          shape: const CircleBorder(),
+          child: const Icon(Icons.navigate_next),
+        ),
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 30),
+              Text(
+                "Enter your mobile number",
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 26,
+                  wordSpacing: 3,
+                  fontFamily: 'Gilroy',
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.none,
+                ),
+                textAlign: TextAlign.left,
+              ),
+              SizedBox(height: 30),
+              Text(
+                "Mobile Number",
+                style: TextStyle(
+                  color: Color.fromRGBO(130, 130, 130, 1),
+                  fontSize: 14,
+                  fontFamily: 'Gilroy',
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.none,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 5),
+              TextField(
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 16,
+                    horizontal: 0,
+                  ),
+                  prefixIcon: Container(
+                    alignment: Alignment.centerLeft,
+                    constraints: BoxConstraints(maxWidth: 78),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 6),
+                          child: Image.asset(
+                            "assets/images/flag.png",
+                            width: 34,
+                            height: 24,
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        Text(
+                          "+880",
+                          style: TextStyle(fontSize: 16, height: 1.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                style: TextStyle(fontSize: 16, height: 1.0),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_groceries/layers/core/presentation/home_page.dart';
 import 'package:flutter_groceries/layers/core/presentation/onbording_screen.dart';
+import 'package:flutter_groceries/layers/core/presentation/verification_screen.dart';
 import 'layers/core/presentation/number_screen.dart';
 import 'layers/core/presentation/sign_in_screen.dart';
 import 'layers/core/presentation/splash_screen.dart';
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: Scaffold(
-        body: NumberScreen(),
+        body: VerificationScreen(),
       ),
       debugShowCheckedModeBanner: false,
     );
